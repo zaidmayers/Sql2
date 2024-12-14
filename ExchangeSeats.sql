22de369bdcd43fa58c9a639b0732a66d47271578
Zaid Roshan Mayers <111454369+zaidmayers@users.noreply.github.com>
2024-12-14 17:00:00 -0500

    Update ExchangeSeats.sql
    Added another way of soving the problem.

--- ExchangeSeats.sql
+++ ExchangeSeats.sql
@@ -7,3 +7,7 @@ SELECT
     student 
 FROM seat, (SELECT COUNT(*) AS cnt FROM seat) AS seat_counts 
 ORDER BY id;
+
+-- Solution 2:
+-- select s1.id, coalesce(s2.student, s1.student) as 'student' from seat s1
+-- left join seat s2 on (s1.id + 1) ^ 1 -1 = s2.id order by s1.id
